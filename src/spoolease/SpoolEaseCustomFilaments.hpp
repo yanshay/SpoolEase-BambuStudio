bd6659a96a4f880addd91081737df8c14c4ad49e
yanshay <yanshay>
2026-06-04 15:33:01 +0300

    Add sync custom filaments to SpoolEase menu

---
+++ src/spoolease/SpoolEaseCustomFilaments.hpp
@@ -0,0 +1,12 @@
+#ifndef spoolEaseCustomFilaments_hpp_
+#define spoolEaseCustomFilaments_hpp_
+
+class wxWindow;
+
+namespace Slic3r { namespace SpoolEase {
+
+void sync_custom_filaments(wxWindow& parent);
+
+}} // namespace Slic3r::SpoolEase
+
+#endif // spoolEaseCustomFilaments_hpp_
